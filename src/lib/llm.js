ab8b754df7225396255331e87c8ae4c35f5d0670
leeonM <110701138+leeonM@users.noreply.github.com>
2024-12-03 23:48:44 +0000

    test o1 model

--- src/lib/llm.js
+++ src/lib/llm.js
@@ -1,14 +1,14 @@
 import { ChatOpenAI } from "@langchain/openai";
 
 export const streamingModel = new ChatOpenAI({
-    modelName: "gpt-4o-mini",
+    modelName: "o1-mini-2024-09-12",
     streaming: true,
     verbose: true,
     temperature: 0.5,
 })
 
 export const nonStreamingModel = new ChatOpenAI({
-    modelName: "gpt-4o-mini",
+    modelName: "o1-mini-2024-09-12",
     verbose: true,
     temperature: 0.5,
 })
